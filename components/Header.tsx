951b4900cb5ce0c35b6e58defe7a3f4f0f7bc6dd
Facu Tom <facu_tomasella@hotmail.com>
2026-02-05 19:32:32 -0300

    x

--- components/Header.tsx
+++ components/Header.tsx
@@ -78,6 +78,11 @@ const Header: React.FC<HeaderProps> = ({
             )}
           </div>
 
+          <a href="https://twitter.com/tradingchango" target="_blank" rel="noopener noreferrer" className="w-8 h-8 flex items-center justify-center text-black dark:text-[#e9edef] hover:opacity-70 transition-opacity">
+            <svg viewBox="0 0 24 24" aria-hidden="true" className="w-5 h-5 fill-current">
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+            </svg>
+          </a>
           <button onClick={onUserClick} className="w-8 h-8 flex items-center justify-center text-black dark:text-[#e9edef]">
             <i className={`fa-solid fa-circle-user text-xl ${isPro ? 'text-green-600' : ''}`}></i>
           </button>
